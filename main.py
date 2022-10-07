434f3b6358ac801b1ec31cba682fd49c5b7b5e83
Мужецкий Антон <tonich2001@gmail.com>
2022-10-07 08:24:17 +0300

    fix: first review

--- main.py
+++ main.py
@@ -2,27 +2,32 @@ from server import HTTPWebServer
 
 HOST = '0.0.0.0'
 PORT = 80
-DEFAULT_THREADS = 256
+DEFAULT_THREAD_NUM = 256
+DEFAULT_CPU_NUM = 4
 DEFAULT_ROOT_DIR = './tests'
 
 
-def parseConf():
-    threads = DEFAULT_THREADS
+def config():
+    threads = DEFAULT_THREAD_NUM
+    cpus = DEFAULT_CPU_NUM
     root = DEFAULT_ROOT_DIR
+
     try:
         f = open('./httpd.conf', 'r')
         parsedFile = f.read().split('\n')
         for text in parsedFile:
+            if text.find('cpu_limit') > -1:
+                cpus = int(text.split(' ')[1])
             if text.find('thread_limit') > -1:
                 threads = int(text.split(' ')[1])
             if text.find('document_root') > -1:
                 root = text.split(' ')[1]
         f.close()
     finally:
-        return threads, root
+        return threads, cpus, root
 
 
 if __name__ == '__main__':
-    numThreads, rootFolder = parseConf()
-    server = HTTPWebServer(HOST, PORT, numThreads, rootFolder)
+    trds, cpus, root = config()
+    server = HTTPWebServer(HOST, PORT, trds, cpus, root)
     server.listenAndServe()
--- server.py
+++ server.py
@@ -9,127 +9,124 @@ import socket
 import signal
 
 
+BAD_REQ_HDR = [('Server', 'Python-thread-pool-server'),
+               ('Date', datetime.now()),
+               ('Connection', 'close')]
+
+
 class HTTPResponse:
     def __init__(self, code, status, headers=None, body=None):
-        self._code = code
-        self._status = status
-        self._headers = headers
-        self._body = body
+        self.code = code
+        self.status = status
+        self.hdr = headers
+        self.body = body
 
 
 class HTTPRequest:
-    def __init__(self, method, path, ver, headers, body):
-        self._method = method
-        self._path = urlparse(path).path
-        self._ver = ver
-        self._headers = headers
-        self._body = body
+    def __init__(self, method, path, headers, body):
+        self.method = method
+        self.url = urlparse(path).path
+        self.hdr = headers
+        self.body = body
 
 
 class HTTPWebServer():
-    def __init__(self, host, port, threads, dir):
-        self._cpuNum = os.cpu_count()
-        self._host = host
-        self._port = port
-        self._threads = threads
-        self._dir = dir
-        self._cpuPool = []
-        self._requestQueue = queue.SimpleQueue()
-        self._badReqHeaders = [('Server', 'Python-thread-pool-server'),
-                               ('Date', datetime.now()),
-                               ('Connection', 'close')]
-
-    def listenAndServe(self):
-        sock = socket.socket()
-        try:
-            sock.bind((self._host, self._port))
-            sock.listen()
-            pid = 0
-            for _ in range(self._cpuNum):
-                pid = os.fork()
-                if pid == 0:
-                    continue
-                for _ in range(self._threads):
-                    t = threading.Thread(target=self.threadWork, daemon=True)
-                    t.start()
-                self._cpuPool.append(pid)
-                print(pid)
-                while True:
-                    conn, _ = sock.accept()
-                    self._requestQueue.put(conn)
-
-            while True:
-                1 # отвисаем
-
-        except KeyboardInterrupt:
-            sock.close()
-            for pid in self._cpuPool:
-                os.kill(pid, signal.SIGTERM)
+    def __init__(self, host, port, threads, cpus, dir):
+        self.cpunum = cpus
+        self.host = host
+        self.port = port
+        self.trd = threads
+        self.root = dir
+        self.pids = []
+        self.reqq = queue.SimpleQueue()
 
     def threadWork(self):
         while True:
-            conn = self._requestQueue.get()
+            conn = self.reqq.get()
             if conn:
                 self.handle(conn)
                 conn.close()
 
-    def fileLookUp(self, conn, filePath):
-        type, _ = mimetypes.guess_type(filePath, strict=True)
-        headers = [('Content-Type', type),
+    def listenAndServe(self):
+        print('> BEGIN')
+        print(f'> config\n\t* cpu: {self.cpunum}\n\t* thread: {self.trd}')
+
+        sock = socket.socket()
+        try:
+            sock.bind((self.host, self.port))
+            sock.listen()
+            pid = 0
+            print('pids:')
+            for _ in range(self.cpunum):
+                pid = os.fork()
+                if pid == 0:
+                    continue
+                for _ in range(self.trd):
+                    t = threading.Thread(target=self.threadWork, daemon=True)
+                    t.start()
+                self.pids.append(pid)
+                print(f'\t{pid}')
+                while True:
+                    conn, _ = sock.accept()
+                    self.reqq.put(conn)
+
+            while True:
+                1  # отвисаем
+
+        except KeyboardInterrupt:
+            sock.close()
+            for pid in self.pids:
+                os.kill(pid, signal.SIGTERM)
+            print('')
+
+    def handle(self, conn):
+        request = self.parseRequest(conn)
+        if (type(request) != HTTPRequest):
+            self.response(conn, request)
+            return
+        if request.url.find('/../') != -1:
+            self.response(conn, HTTPResponse(403, 'Forbidden', BAD_REQ_HDR))
+            return
+        indexFile = False
+        unquotedPath = unquote(request.url)
+        if request.url[-1] == '/' and request.url.find('.') == -1:
+            filePath = self.root + unquotedPath + 'index.html'
+            indexFile = True
+        else:
+            filePath = self.root + unquotedPath
+
+        try:
+            file = open(filePath, 'rb')
+        except:
+            if indexFile:
+                resp = HTTPResponse(403, 'Forbidden', BAD_REQ_HDR)
+            else:
+                resp = HTTPResponse(404, 'Not Found', BAD_REQ_HDR)
+            self.response(conn, resp)
+            return
+
+        t, _ = mimetypes.guess_type(filePath, strict=True)
+        headers = [('Content-Type', t),
                    ('Content-Length', os.path.getsize(filePath)),
                    ('Server', 'Python-thread-pool-server'),
                    ('Date', datetime.now()),
                    ('Connection', 'close')]
         self.response(conn, HTTPResponse(200, 'OK', headers))
 
-    def handle(self, conn):
-        request = self.parseRequest(conn)
-        if (type(request) != HTTPRequest):
-            # отправляем ошибкой, если произошла ошибка парсинга
-            self.response(conn, request)
-            return
-        if request._path.find('/../') != -1:
-            self.response(conn,
-                          HTTPResponse(403, 'Forbidden', self._badReqHeaders))
-            return
-        indexFile = False
-        unquotedPath = unquote(request._path)
-        if request._path[-1] == '/' and request._path.find('.') == -1:
-            filePath = self._dir + unquotedPath + 'index.html'
-            indexFile = True
-        else:
-            filePath = self._dir + unquotedPath
-
-        try:
-            file = open(filePath, 'rb')
-        except:
-            if indexFile:
-                resp = HTTPResponse(
-                    403, 'Forbidden', headers=self._badReqHeaders)
-            else:
-                resp = HTTPResponse(
-                    404, 'Not Found', headers=self._badReqHeaders)
-            self.response(conn, resp)
-            return
-
-        self.fileLookUp(conn, filePath)
-        if request._method == 'GET':
-            try:
-                conn.sendfile(file)
-            except BrokenPipeError:
-                conn.sendfile(file)
+        if request.method == 'GET':
+            conn.sendfile(file)
         file.close()
 
     def response(self, conn, res):
         answer = conn.makefile('w')
-        answer.write(f'HTTP/1.1 {res._code} {res._status}\r\n')
+        answer.write(f'HTTP/1.1 {res.code} {res.status}\r\n')
 
-        if res._headers:
-            for (key, value) in res._headers:
+        if res.hdr:
+            for (key, value) in res.hdr:
                 answer.write(f'{key}: {value}\r\n')
         answer.write('\r\n')
 
-        if res._body:
+        if res.body:
             answer.write(res.body)
         answer.close()
 
@@ -137,12 +134,12 @@ class HTTPWebServer():
         rawFile = conn.makefile('r')
         info = rawFile.readline().split()
         if len(info) != 3:
-            return HTTPResponse(400, 'Bad request', self._badReqHeaders)
+            return HTTPResponse(400, 'Bad request', BAD_REQ_HDR)
         method, path, ver = info
         if method != 'GET' and method != 'HEAD':
-            return HTTPResponse(405, 'Method Not Allowed', self._badReqHeaders)
+            return HTTPResponse(405, 'Method Not Allowed', BAD_REQ_HDR)
         if ver != 'HTTP/1.1' and ver != 'HTTP/1.0':
-            return HTTPResponse(505, 'HTTP Version Not Supported', self._badReqHeaders)
+            return HTTPResponse(505, 'HTTP Version Not Supported', BAD_REQ_HDR)
         headers = {}
         while True:
             line = rawFile.readline()
@@ -151,10 +148,10 @@ class HTTPWebServer():
             headerName, headerValue = line.split(':')
             headers[headerName] = headerValue
         if 'Content-length' in headers:
-            req = HTTPRequest(method, path, ver, headers,
+            req = HTTPRequest(method, path, headers,
                               rawFile.read(int(headers['Content-length'])))
             rawFile.close()
             return req
         else:
             rawFile.close()
-            return HTTPRequest(method, path, ver, headers, None)
+            return HTTPRequest(method, path, headers, None)
